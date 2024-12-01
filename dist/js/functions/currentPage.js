3f10b496750e44e705b22b6ac0eb9b2f19576f6d
andreyk1n <betas23wr@gmail.com>
2024-12-01 13:22:17 +0200

    update currentPage.js

--- dist/js/functions/currentPage.js
+++ dist/js/functions/currentPage.js
@@ -1,9 +1,8 @@
 export function currentPage() {
-    const currentLink = document.querySelectorAll('.nav__link'); // клас посилання
-    const currentPath = window.location.pathname.split('/').pop();
-    currentLink.forEach(link => {
-        if (link.getAttribute('href') === currentPath) {
-            link.classList.add('currentPage');
-        }
+
+    const currentPath = window.location.pathname.split('/').pop() || 'index.html';
+    document.querySelectorAll('.nav__link').forEach(link => {
+        if (link.getAttribute('href') === currentPath) link.classList.add('currentPage');
     });
+
 }
