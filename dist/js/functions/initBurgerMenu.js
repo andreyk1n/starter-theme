03ea1437c9e7bbb43c809f863e810510a2db1a5c
andreyk1n <betas23wr@gmail.com>
2025-01-06 19:58:59 +0200

    add toggleActive.js

--- dist/js/functions/initBurgerMenu.js
+++ dist/js/functions/initBurgerMenu.js
@@ -2,31 +2,6 @@
 // Функціонал для бургер меню. 
 // ---------------------------------------------------------------------------------------------------------------
 
-// export function initBurgerMenu() {
-//     document.addEventListener('DOMContentLoaded', () => {
-//         // ---------------------------------------------------------------------------------------------------------------
-//         // Класи до яких ми будемо доступатись
-//         // ---------------------------------------------------------------------------------------------------------------
-//         const burger = document.querySelector('.header__burger');
-//         const nav = document.querySelector('.header__nav');
-//         const body = document.body; // Доступ до body
-
-//         // ---------------------------------------------------------------------------------------------------------------
-//         // При кліці на бургер додаємо клас .active для .header__burger, .header__nav та блокуємо скрол сторінки
-//         // ---------------------------------------------------------------------------------------------------------------
-//         burger.addEventListener('click', () => {
-//             burger.classList.toggle('active');
-//             nav.classList.toggle('active');
-
-//             if (burger.classList.contains('active')) {
-//                 body.classList.add('no-scroll'); // Заборона скролу
-//             } else {
-//                 body.classList.remove('no-scroll'); // Відновлення скролу
-//             }
-//         });
-//     });
-// }
-
 // beta version
 export function initBurgerMenu() {
     document.addEventListener('DOMContentLoaded', () => {
