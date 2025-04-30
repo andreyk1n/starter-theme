ba558b531e12a5f12b28e3f454b6a3924db30aaf
andreyk1n <betas23wr@gmail.com>
2025-04-30 19:55:31 +0300

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,69 @@
+// const gulp = require('gulp');
+// const fileInclude = require('gulp-file-include');
+// const sass = require('gulp-sass')(require('sass'));
+// const cleanCSS = require('gulp-clean-css');
+// const rename = require('gulp-rename');
+// const browserSync = require('browser-sync').create();
+
+// const paths = {
+//   html: {
+//     src: 'src/pages/*.html',
+//     dest: 'dist/'
+//   },
+//   styles: {
+//     src: 'src/scss/**/*.scss',
+//     dest: 'dist/css/'
+//   }
+// };
+
+// // HTML Task
+// function html() {
+//   return gulp.src(paths.html.src)
+//     .pipe(fileInclude({
+//       prefix: '@@',
+//       basepath: 'src/partials/'
+//     }))
+//     .pipe(gulp.dest(paths.html.dest))
+//     .pipe(browserSync.stream());
+// }
+
+// // Styles Task
+// function styles() {
+//   return gulp.src(paths.styles.src)
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(cleanCSS())
+//     .pipe(rename({ suffix: '.min' }))
+//     .pipe(gulp.dest(paths.styles.dest))
+//     .pipe(browserSync.stream());
+// }
+
+// // Watch Task
+// function watch() {
+//   browserSync.init({
+//     server: {
+//       baseDir: './dist'
+//     }
+//   });
+//   gulp.watch(paths.html.src, html);
+//   gulp.watch('src/partials/*.html', html);
+//   gulp.watch(paths.styles.src, styles);
+// }
+
+// // Default Task
+// exports.default = gulp.series(gulp.parallel(html, styles), watch);
+
+
+// === ІМПОРТ ПЛАГІНІВ ===
 const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify');
+const plumber = require('gulp-plumber');
 
+// === ШЛЯХИ ДО ФАЙЛІВ ===
 const paths = {
   html: {
     src: 'src/pages/*.html',
@@ -13,11 +72,18 @@ const paths = {
   styles: {
     src: 'src/scss/**/*.scss',
     dest: 'dist/css/'
+  },
+  scripts: {
+    app: 'src/js/app.js',
+    appDest: 'dist/js/',
+    functions: 'src/js/functions/**/*.js',
+    functionsDest: 'dist/js/functions/'
   }
 };
 
-// HTML Task
+// === ОБРОБКА HTML З FILE INCLUDE ===
 function html() {
+  console.log('\x1b[36m%s\x1b[0m', '📄 HTML обробляється...');
   return gulp.src(paths.html.src)
     .pipe(fileInclude({
       prefix: '@@',
@@ -27,8 +93,9 @@ function html() {
     .pipe(browserSync.stream());
 }
 
-// Styles Task
+// === ОБРОБКА SCSS У СТИЛІ ===
 function styles() {
+  console.log('\x1b[35m%s\x1b[0m', '🎨 SCSS компілюється...');
   return gulp.src(paths.styles.src)
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
@@ -37,17 +104,42 @@ function styles() {
     .pipe(browserSync.stream());
 }
 
-// Watch Task
+// === КОПІЮВАННЯ app.js БЕЗ МІНІМІЗАЦІЇ ===
+function jsApp() {
+  console.log('\x1b[33m%s\x1b[0m', '🧩 jsApp копіюється без мінімізації...');
+  return gulp.src(paths.scripts.app)
+    .pipe(gulp.dest(paths.scripts.appDest))
+    .pipe(browserSync.stream());
+}
+
+// === МІНІМІЗАЦІЯ functions/*.js ===
+function jsFunctions() {
+  console.log('\x1b[32m%s\x1b[0m', '⚙️ jsFunctions мінімізується...');
+  return gulp.src(paths.scripts.functions)
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.functionsDest))
+    .pipe(browserSync.stream());
+}
+
+// === СПОСТЕРЕЖЕННЯ ЗА ЗМІНАМИ + BROWSERSYNC ===
 function watch() {
   browserSync.init({
     server: {
       baseDir: './dist'
     }
   });
+
   gulp.watch(paths.html.src, html);
   gulp.watch('src/partials/*.html', html);
   gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.scripts.app, jsApp);
+  gulp.watch(paths.scripts.functions, jsFunctions);
+  console.log('\x1b[44m%s\x1b[0m', '👀 Gulp слідкує за файлами...');
 }
 
-// Default Task
-exports.default = gulp.series(gulp.parallel(html, styles), watch);
+// === ЕКСПОРТ ЗАДАЧ ===
+exports.default = gulp.series(
+  gulp.parallel(html, styles, jsApp, jsFunctions),
+  watch
+);
