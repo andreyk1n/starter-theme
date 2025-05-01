b751dc510829311ccd8a5d943d23e253833d66ba
andreyk1n <betas23wr@gmail.com>
2025-05-01 18:51:02 +0300

    clear

--- src/js/functions/initBurgerMenu.js
+++ src/js/functions/initBurgerMenu.js
@@ -41,34 +41,3 @@ export function initBurgerMenu() {
         });
     });
 }
-
-// ---------------------------------------------------------------------------------------------------------------
-
-// min code version for delpoy
-
-// export function initBurgerMenu() {
-//     document.addEventListener('DOMContentLoaded', () => {
-//         const burger = document.querySelector('.header__burger');
-//         const nav = document.querySelector('.header__nav');
-//         const body = document.body;
-
-//         const toggleMenu = () => {
-//             const isActive = burger.classList.toggle('active');
-//             nav.classList.toggle('active', isActive);
-//             body.classList.toggle('no-scroll', isActive);
-//         };
-
-//         burger.addEventListener('click', (e) => {
-//             e.stopPropagation();
-//             toggleMenu();
-//         });
-
-//         document.addEventListener('click', (e) => {
-//             if (!nav.contains(e.target) && !burger.contains(e.target)) {
-//                 burger.classList.remove('active');
-//                 nav.classList.remove('active');
-//                 body.classList.remove('no-scroll');
-//             }
-//         });
-//     });
-// }
